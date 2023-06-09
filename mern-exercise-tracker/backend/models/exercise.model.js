b126fa1ff69016a66e8dfad35f498fb05e47fc90
Danny <cool.huip@gmail.com>
2023-06-09 04:37:56 -0400

    Update exercise.model.js

--- mern-exercise-tracker/backend/models/exercise.model.js
+++ mern-exercise-tracker/backend/models/exercise.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const exerciseSchema = new Schema({
     username: { type: String, required: true },
     description: { type: String, required: true },
     duration: { type: Number, required: true },
@@ -11,6 +11,6 @@ const userSchema = new Schema({
     timestamps: true,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('Exercise', exerciseSchema);
 
 module.exports = User;
