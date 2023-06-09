796bc1d808ab83f3bce9492b93b0df804c7c78a0
Danny <cool.huip@gmail.com>
2023-06-09 03:33:57 -0400

    Update server.js

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -9,7 +9,8 @@ const app = express();  // This is how we will
 const port = process.env.PORT || 5000;  // This is the port that
                                         // our server will be on.
 
-app.use(cors());    // This is the cors middleware.
+app.use(cors());    // This is the CORS middleware.
+                    // CORS = Cross-Origin Resource Sharing
 app.use(express.json());    // This allows us to parse JSON, since our
                             // server will be receiving AND sending JSON.
 
