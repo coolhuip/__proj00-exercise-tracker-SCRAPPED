1ce7f40d1cee42e4724b618d68f09ff2dbb8cb8b
Danny <cool.huip@gmail.com>
2023-06-12 01:58:30 -0400

    Create React component files

--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -1,28 +1,27 @@
-
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
 
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className=" navbar navbar-dark bg-dark navbar-expand-lg">ExcerTracker</Link>
-        <div className="Collapse navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li>
-        </ul>
-        </div>
-      </nav>
-    )
-  }
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className=" navbar navbar-dark bg-dark navbar-expand-lg">ExcerTracker</Link>
+                <div className="Collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="navbar-item">
+                    <Link to="/" className="nav-link">Exercises</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Create Exercise Log</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/user" className="nav-link">Create User</Link>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        )
+    }
 
 }
